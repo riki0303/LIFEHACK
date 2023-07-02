424f390ef0eea6cc0e69ac5c0e5fd401e9a19d52
小久保力輝 <kokub0303@gmail.com>
2023-07-02 19:30:31 +0900

    記事を投稿できるように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,21 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!
-  
+
+  # def index
+  #   @posts = Post.all
+  # end
+
   def new
     @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
