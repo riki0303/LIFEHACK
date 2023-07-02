5a431fd817e5dd66eab253000f02676b7d0b0222
小久保力輝 <kokub0303@gmail.com>
2023-07-02 19:00:17 +0900

    投稿をnew出来るように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  
   def new
-    
+    @post = current_user.posts.build
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
