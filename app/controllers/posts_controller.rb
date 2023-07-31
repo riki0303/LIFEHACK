713da88169ccbf782067a8e129e82d3b93e0e38d
小久保力輝 <kokub0303@gmail.com>
2023-07-31 12:23:36 +0900

    記事を更新出来るように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_path, notice: '更新出来ました'
+    else
+      flash.now[:error] = '更新出来ませんでした'
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
