9d7560fde127a206a4d1dad903c6c8e42cf34b76
小久保力輝 <kokub0303@gmail.com>
2023-08-03 07:47:35 +0900

    投稿内容の文字制限

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,5 +21,5 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :content, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
