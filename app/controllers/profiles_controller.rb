1ee17e5c1aa508407b4ccf00d8b53636b300978c
小久保力輝 <kokub0303@gmail.com>
2023-08-11 10:56:49 +0900

    editとupdateだけでプロフィールを作成・更新出来るように

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,12 +4,14 @@ class ProfilesController < ApplicationController
     @profile = current_user.profile
   end
 
+  # new,create省略出来るが、profileが存在する場合と存在しない場合を意識して処理を書く必要がある
   def edit
-    @profile = current_user.build_profile
+    @profile = current_user.prepare_profile
   end
 
   def update
-    @profile = current_user.build_profile(profile_params)
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to profile_path, notice: '更新出来ました'
     else
