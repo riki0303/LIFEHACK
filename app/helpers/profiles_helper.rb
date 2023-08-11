593bc4d45739ff680c8bc2ff5be9a83dc4327cf3
小久保力輝 <kokub0303@gmail.com>
2023-08-11 11:43:08 +0900

    プロフィール画像を設定していなくてもデフォルト画像が表示されるように

---
+++ app/helpers/profiles_helper.rb
@@ -0,0 +1,9 @@
+module ProfilesHelper
+  def display_profile_avatar(profile)
+    if profile.present? && profile.avatar.attached?
+      image_tag profile.avatar
+    else
+      image_tag 'noavatar.png'
+    end
+  end
+end
