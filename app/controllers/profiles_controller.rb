edc927d336884b6c8260c52cd6338eb4ebfbce10
小久保力輝 <kokub0303@gmail.com>
2023-08-11 10:36:21 +0900

    プロフィール作成できるように

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,6 +5,21 @@ class ProfilesController < ApplicationController
   end
 
   def edit
+    @profile = current_user.build_profile
   end
 
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: '更新出来ました'
+    else
+      flash.now[:error] = '更新出来ませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:name, :bio, :avatar)
+  end
 end
