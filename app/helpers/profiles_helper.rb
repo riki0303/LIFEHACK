e91c12218fd1da32a1fb563d8603b899bcf5bc2e
小久保力輝 <kokub0303@gmail.com>
2023-08-12 11:57:07 +0900

    メソッド修正

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -1,9 +1,9 @@
 module ProfilesHelper
-  def display_profile_avatar(profile)
+  def display_profile_avatar(profile,css_class)
     if profile&.avatar.attached?
-      image_tag profile.avatar, class: 'profile__avatar'
+      image_tag profile.avatar, class: css_class
     else
-      image_tag 'noavatar.png', class: 'profile__avatar'
+      image_tag 'noavatar.png', class: css_class
     end
   end
 end
