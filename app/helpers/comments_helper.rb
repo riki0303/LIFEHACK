4d679fc74edec7bd0deb2195d519ed89e9217b93
小久保力輝 <kokub0303@gmail.com>
2023-08-13 11:54:29 +0900

    いいね数、コメント数表示されるように

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -2,4 +2,8 @@ module CommentsHelper
   def my_comment?(comment)
     comment.user == current_user
   end
+
+  def comments_count(post)
+    post.comments.count
+  end
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -2,4 +2,8 @@ module LikesHelper
   def has_liked?(user, post)
     user.likes.exists?(post_id: post.id)
   end
+
+  def likes_count(post)
+    post.likes.count
+  end
 end
