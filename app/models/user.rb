473dee8e0f926757cb22ace02a498147e1dd15ce
小久保力輝 <kokub0303@gmail.com>
2023-08-15 11:00:54 +0900

    フォロワーの関係を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,10 @@ class User < ApplicationRecord
   has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :followings, through: :following_relationships, source: :following
 
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :follower, through: :follower_relationships, source: :follower
+
+
   def prepare_profile
     profile || build_profile
   end
