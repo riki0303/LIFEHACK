abb4024d5bd3fb894216916c2983118c91c0c3f3
小久保力輝 <kokub0303@gmail.com>
2023-09-07 10:06:21 +0900

    タイムラインでのN+1改善

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,6 +2,6 @@ class TimelinesController < ApplicationController
   before_action :authenticate_user!
   def show
     user_ids = current_user.followings.pluck(:id)
-    @posts = Post.where(user_id: user_ids )
+    @posts = Post.where(user_id: user_ids ).includes(user: { profile: :avatar_attachment }).with_attached_image
   end
 end
