33e3cdb84c6cc2d59c4b46c5be3334df35182679
小久保力輝 <kokub0303@gmail.com>
2023-09-07 16:46:14 +0900

    投稿をid順→作成日順へ

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,6 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.favorites.includes(user: { profile: :avatar_attachment }).with_attached_image
+    @posts = current_user.favorites.includes(user: { profile: :avatar_attachment }).with_attached_image.order(created_at: 'DESC')
   end
 end
