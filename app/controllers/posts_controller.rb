c7c1ffb00711703ac9ebfd3a771f0d6fe93dada8
小久保力輝 <kokub0303@gmail.com>
2023-09-08 07:51:30 +0900

    投稿検索ページでのページネーション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   def search
     @q = Post.ransack(params[:q])
     if params[:q].present?
-      @posts = @q.result(distinct: true).includes(user: { profile: :avatar_attachment }).with_attached_image.order(created_at: 'DESC')
+      @posts = @q.result(distinct: true).includes(user: { profile: :avatar_attachment }).with_attached_image.page(params[:page]).order(created_at: 'DESC')
     else
       @posts = Post.none # 空の結果を設定
     end
