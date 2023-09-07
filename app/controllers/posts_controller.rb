ff7446ed27cde64828240c0dfc1e34ac324c7d82
小久保力輝 <kokub0303@gmail.com>
2023-09-07 18:10:11 +0900

    投稿ページ一覧が10件以上であればページネーションが表示されるように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all.includes(user: { profile: :avatar_attachment }).with_attached_image.order(created_at: 'DESC')
+    @posts = Post.all.includes(user: { profile: :avatar_attachment }).with_attached_image.page(params[:page]).order(created_at: 'DESC')
     @q = Post.ransack(params[:q])
   end
 
