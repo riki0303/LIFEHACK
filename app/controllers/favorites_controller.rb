d0b298f26a1de95696e7c040a57f169c96c34ddf
小久保力輝 <kokub0303@gmail.com>
2023-09-08 07:51:54 +0900

    いいね一覧ページへのページネーション追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,6 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.favorites.includes(user: { profile: :avatar_attachment }).with_attached_image.order(created_at: 'DESC')
+    @posts = current_user.favorites.includes(user: { profile: :avatar_attachment }).with_attached_image.page(params[:page]).order(created_at: 'DESC')
   end
 end
