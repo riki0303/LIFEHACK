11213ef68eeb51a73dd95a5fd3ad2b66d60f618a
小久保力輝 <kokub0303@gmail.com>
2023-10-09 18:30:00 +0900

    postのタイトルが未入力の場合・31文字以上の場合のmodel_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,19 +6,33 @@ RSpec.describe Post, type: :model do
   context 'タイトル、内容、画像が入力されている場合'
     let!(:post)  { build(:post, user: user)}
 
-    it 'タイトルと内容が入力されていれば投稿を保存できる' do
+    it '投稿を保存できる' do
       expect(post).to be_valid
     end
 
-    context 'タイトルが40文字の場合' do
-      let!(:post)  { build(:post, title:Faker::Lorem.characters(number: 40),  user: user)}
+    describe 'title' do
+      context 'titleが未入力の場合' do
+        let!(:post)  { build(:post, title:'',  user: user)}
 
-      before do
-        post.save
+        before do
+          post.save
+        end
+
+        it '投稿を保存できない' do
+          expect(post).to_not be_valid
+        end
       end
 
-      it '投稿を保存できない' do
-        expect(post.errors.messages[:title][0]).to eq('は30文字以内で入力してください')
+      context 'titleが31文字以上の場合' do
+        let!(:post)  { build(:post, title:Faker::Lorem.characters(number: 40),  user: user)}
+
+        before do
+          post.save
+        end
+
+        it '投稿を保存できない' do
+          expect(post).to_not be_valid
+        end
       end
     end
 end
