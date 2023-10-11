a57e112e0761147fdedfd9e428cf04dbc9dd0b6a
小久保力輝 <kokub0303@gmail.com>
2023-10-11 16:38:28 +0900

    system_specで記事を投稿出来るかテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     account { Faker::Lorem.characters(number: 10) }
     email { Faker::Internet.email }
     password { 'password' }
+    password_confirmation { 'password' }
   end
 end
