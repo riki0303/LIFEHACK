b78dcf459d2acd111886fec054815b69d9baaa81
小久保力輝 <kokub0303@gmail.com>
2023-10-15 20:38:26 +0900

    プロフィールペーに、名前・自己紹介・プロフィール画像が表示されている事をテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     email { Faker::Internet.email }
     password { 'password' }
     password_confirmation { 'password' }
+
+    trait :with_profile do
+      after :build do |user|
+        build(:profile, user: user)
+      end
+    end
   end
 end
