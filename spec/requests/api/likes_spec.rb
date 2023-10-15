a108ad1fb71fb647aed20ea04dd6ec643529804e
小久保力輝 <kokub0303@gmail.com>
2023-10-15 19:25:38 +0900

    like_controller show,destroyのテスト(createはエラー解決できず保留)

--- spec/requests/api/likes_spec.rb
+++ spec/requests/api/likes_spec.rb
@@ -3,17 +3,38 @@ require 'rails_helper'
 RSpec.describe 'Api::Likes', type: :request do
   let!(:user) { create(:user) }
   let!(:post) { create(:post, user: user) }
-  let!(:like) { create(:like, user: user, post: post) }
 
-  describe 'GET /api/likes' do
-    context 'ログインしている場合' do
-      before do
-        sign_in user
-      end
+  context 'ログインしている場合' do
+    before do
+      sign_in user
+    end
+
+    describe 'GET /api/likes' do
+      let!(:like) { create(:like, user: user, post: post) }
       it 'JSONを取得できる' do
-        get post_like_path(post_id = post.id)
+        get post_like_path(post.id)
         body = JSON.parse(response.body)
-        expect(body).to eq({'hasLiked'=>true})
+        expect(response.status).to eq(200)
+        expect(body).to eq({ 'hasLiked'=>true })
+      end
+    end
+
+    # describe 'POST /api/like' do
+    #   it 'いいね出来る' do
+    #     post post_like_path(post.id)
+    #     body = JSON.parse(response.body)
+    #     expect(response.status).to eq(200)
+    #     expect(body).to eq({ 'likesCount'=>1, 'status'=>'ok' })
+    #   end
+    # end
+
+    describe 'DELETE /api/likes' do
+      let!(:like) { create(:like, user: user, post: post) }
+      it 'いいねを削除できる' do
+        delete post_like_path(post.id)
+        body = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(body).to eq({ 'likesCount'=>0, 'status'=>'ok' })
       end
     end
   end
