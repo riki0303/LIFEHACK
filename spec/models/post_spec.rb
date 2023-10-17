ee726006b3fc632ab6e7579f0147345817d16f65
小久保力輝 <kokub0303@gmail.com>
2023-10-17 11:06:57 +0900

    リファクタリング

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   let!(:user) { create(:user)}
+  let!(:post)  { build(:post, user: user)}
+
 
   context 'タイトル、内容、画像が入力されている場合' do
-    let!(:post)  { build(:post, user: user)}
-
     it '投稿を保存できる' do
       expect(post).to be_valid
     end
@@ -13,9 +13,8 @@ RSpec.describe Post, type: :model do
 
     describe 'title' do
       context 'titleが未入力の場合' do
-        let!(:post)  { build(:post, title:'',  user: user)}
-
         before do
+          post.title = nil
           post.save
         end
 
@@ -25,9 +24,8 @@ RSpec.describe Post, type: :model do
       end
 
       context 'titleが31文字以上の場合' do
-        let!(:post)  { build(:post, title:Faker::Lorem.characters(number: 40),  user: user)}
-
         before do
+          post.title = Faker::Lorem.characters(number: 40)
           post.save
         end
 
@@ -39,9 +37,8 @@ RSpec.describe Post, type: :model do
 
     describe 'content' do
       context 'contentが未入力の場合' do
-        let!(:post)  { build(:post, content:'',  user: user)}
-
         before do
+          post.content = nil
           post.save
         end
 
@@ -51,9 +48,8 @@ RSpec.describe Post, type: :model do
       end
 
       context 'contenteが141文字以上の場合' do
-        let!(:post)  { build(:post, content:Faker::Lorem.characters(number: 150),  user: user)}
-
         before do
+          post.content = Faker::Lorem.characters(number: 150)
           post.save
         end
 
@@ -65,11 +61,12 @@ RSpec.describe Post, type: :model do
 
     describe 'image' do
       context 'imageが未入力の場合' do
-        user = User.create(account: 'account', email: 'test@test.com', password: 'password')
-        post = user.posts.build(title: 'Sample Post', content: 'This is a sample post content.')
-
+        before do
+          post.image = nil
+          post.save
+        end
         it '投稿を保存できない' do
-          expect(post).to_not be_valid
+          expect(post.errors.full_messages).to eq(['画像を入力してください'])
         end
       end
     end
