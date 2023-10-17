6ccf57eb5a4eec0e2c4634de1df17039eca7d254
小久保力輝 <kokub0303@gmail.com>
2023-10-17 12:17:50 +0900

    post_controllerのcreateアクションのテストコード

--- spec/requests/api/likes_spec.rb
+++ spec/requests/api/likes_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Api::Likes', type: :request do
   let!(:user) { create(:user) }
-  let!(:post) { create(:post, user: user) }
+  # 下記post→new_postと定義
+  # createにおけるpostリクエストとpostインスタンスが重複してしまう為
+  let!(:new_post) { create(:post, user: user) }
 
   context 'ログインしている場合' do
     before do
@@ -10,28 +12,28 @@ RSpec.describe 'Api::Likes', type: :request do
     end
 
     describe 'GET /api/likes' do
-      let!(:like) { create(:like, user: user, post: post) }
+      let!(:like) { create(:like, user: user, post: new_post) }
       it 'JSONを取得できる' do
-        get post_like_path(post.id)
+        get post_like_path(new_post.id)
         body = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(body).to eq({ 'hasLiked'=>true })
       end
     end
 
-    # describe 'POST /api/like' do
-    #   it 'いいね出来る' do
-    #     post post_like_path(post.id)
-    #     body = JSON.parse(response.body)
-    #     expect(response.status).to eq(200)
-    #     expect(body).to eq({ 'likesCount'=>1, 'status'=>'ok' })
-    #   end
-    # end
+    describe 'POST /api/like' do
+      it 'いいね出来る' do
+        post post_like_path(new_post.id)
+        body = JSON.parse(response.body)
+        expect(response.status).to eq(200)
+        expect(body).to eq({ 'likesCount'=>1, 'status'=>'ok' })
+      end
+    end
 
     describe 'DELETE /api/likes' do
-      let!(:like) { create(:like, user: user, post: post) }
+      let!(:like) { create(:like, user: user, post: new_post) }
       it 'いいねを削除できる' do
-        delete post_like_path(post.id)
+        delete post_like_path(new_post.id)
         body = JSON.parse(response.body)
         expect(response.status).to eq(200)
         expect(body).to eq({ 'likesCount'=>0, 'status'=>'ok' })
