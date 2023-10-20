38fc96b44f3ffddb7801d75ed2148dbdf2889efd
小久保力輝 <kokub0303@gmail.com>
2023-10-20 17:10:00 +0900

    自分の記事は編集出来るが、他人の記事は編集出来ない事をテスト

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
   let!(:user) { create(:user) }
-  let!(:posts) { create_list(:post, 3, user: user) }
+  let!(:post) { create(:post, user: user) }
 
   context 'ログインしている場合' do
     before do
@@ -10,18 +10,36 @@ RSpec.describe 'Post', type: :system do
     end
     it '記事一覧が表示される' do
       visit posts_path
-      posts.each do |post|
-        expect(page).to have_css('.post__image')
-        expect(page).to have_css('.post__title', text: post.title)
-        expect(page).to have_css('.post__content', text: post.content)
-      end
+      expect(page).to have_css('.post__image')
+      expect(page).to have_css('.post__title', text: post.title)
+      expect(page).to have_css('.post__content', text: post.content)
     end
 
-    it '自分の記事の編集ボタンがクリックできる' do
-      visit posts_path
-      first('.post__tab').click
-      click_on('編集')
-      expect(current_path).to eq edit_post_path(posts.last.id)
+
+
+    describe '投稿の編集' do
+      context '自分の投稿の場合' do
+        it '記事の編集・削除ボタンが表示されクリックできる' do
+          visit posts_path
+          find('.post__tab').click
+          click_on('編集')
+          expect(current_path).to eq edit_post_path(post.id)
+        end
+      end
+
+      context '他人の投稿の場合' do
+        let!(:user2) { create(:user) }
+        let!(:user2_post) { create(:post, user: user2) }
+
+        before { post.destroy }
+        it '他人の記事の編集・削除ボタンはクリックできない' do
+          visit posts_path
+          expect {
+            find('.post__tab').click
+          }.to raise_error(Capybara::ElementNotFound)
+          expect(current_path).to eq posts_path
+        end
+      end
     end
   end
 end
