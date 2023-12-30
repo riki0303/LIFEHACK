ab2167d71819b27cbd819d4400bbee507aa7472f
小久保力輝 <kokub0303@gmail.com>
2023-12-30 23:29:42 +0900

    tabが開いている場合、tab以外の場所をクリックするとtabが閉じるように(UXの向上)

--- app/javascript/packs/tab.js
+++ app/javascript/packs/tab.js
@@ -5,7 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     $('.header__nav').toggleClass('hidden');
   });
 
+  $(document).on('click', (event) => {
+    // クリックされた要素がheader__tabでない場合
+    if (!$(event.target).closest('.header__tab').length) {
+      // header__navにhiddenクラスを追加
+      $('.header__nav').addClass('hidden');
+    }
+  });
+// ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
   $('.post__tab').on('click', function () {
     $(this).children().toggleClass('hidden');
+  });
+
+  $(document).on('click', function (event) {
+    if (!$(event.target).closest('.post__tab').length) {
+      // .post__tab以外の要素がクリックされた場合、.hiddenクラスを削除
+      $('.post__tab').children().addClass('hidden');
+    }
   });
 });
